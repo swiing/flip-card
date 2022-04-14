6bca2b46bcdfbe8f3a4a1af07f4a00211bcef252
swiing <persovince@yahoo.fr>
2022-04-14 18:15:09 +0200

    refactor: simplify

--- src/flip-panel.js
+++ src/flip-panel.js
@@ -1,44 +1,41 @@
 // SPDX-License-Identifier: ISC
 
-customElements.define(
-  'flip-panel',
-  class extends HTMLElement {
-    // Don't add children in constructor, hence prefer connectedCallback as per
-    // https://stackoverflow.com/a/40181684
-    connectedCallback() {
-      const content = document
-        .createRange()
-        .createContextualFragment('<div is="flipper"></div>');
+export default class extends HTMLElement {
+  // Don't add children in constructor, hence prefer connectedCallback as per
+  // https://stackoverflow.com/a/40181684
+  connectedCallback() {
+    const content = document
+      .createRange()
+      .createContextualFragment('<div is="flipper"></div>');
 
-      // populate the template with children from the custom element
-      while (this.firstElementChild) {
-        content
-          .querySelector('[is=flipper]')
-          .appendChild(this.removeChild(this.firstElementChild));
-      }
-
-      // make back-side unfocusable (un-tab-able)
-      content.querySelector('x-card:nth-of-type(2)').classList.toggle('hidden');
-
-      this.appendChild(content);
+    // populate the template with children from the custom element
+    while (this.firstElementChild) {
+      content
+        .querySelector('[is=flipper]')
+        .appendChild(this.removeChild(this.firstElementChild));
     }
 
-    flip() {
-      const flipper = this.querySelector('[is=flipper]');
-      flipper.classList.toggle('flipped');
-      setTimeout(() => {
-        // hide element so that it does not get focus (e.g. with tabindex)
-        flipper
-          .querySelectorAll('x-card:nth-of-type(-n+2)')
-          .forEach((xcard) => {
-            xcard.classList.toggle('hidden');
-          });
-        // timer value is taken from css transition time. I could do it
-        // programmatically with something using
-        // getComputedStyle(document.querySelector('x-card')).transition
-        // => "all 1s ease 0s". Alternatively, I could use a config var
-        // and use it both in the (string) css, and here.
-      }, 500);
-    }
+    // make back-side unfocusable (un-tab-able)
+    content.querySelector('back-face').classList.toggle('hidden');
+
+    this.appendChild(content);
   }
-);
+
+  flip() {
+    const flipper = this.querySelector('[is=flipper]');
+    flipper.classList.toggle('flipped');
+    // setTimeout(() => {
+    //   // hide element so that it does not get focus (e.g. with tabindex)
+    //   flipper
+    //     .querySelectorAll('back-face')
+    //     .forEach((xcard) => {
+    //       xcard.classList.toggle('hidden');
+    //     });
+    //   // timer value is taken from css transition time. I could do it
+    //   // programmatically with something using
+    //   // getComputedStyle(document.querySelector('x-card')).transition
+    //   // => "all 1s ease 0s". Alternatively, I could use a config var
+    //   // and use it both in the (string) css, and here.
+    // }, 1000);
+  }
+}
--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 // SPDX-License-Identifier: ISC
 
-import './flip-panel.js';
+import FlipPanel from './flip-panel.js';
 import sheet from './flip-panel.css' assert { type: 'css' };
 
 document.adoptedStyleSheets = [sheet];
+
+customElements.define('flip-panel', FlipPanel);
